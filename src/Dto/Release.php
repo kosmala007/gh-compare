9c92d8d72375644ea9ab0e7bf2da7f80b809076b
Marcin Kosmala <kosmala007@gmail.com>
2021-12-19 19:25:11 +0100

    Add missing methods to Release dto

--- src/Dto/Release.php
+++ src/Dto/Release.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
+use DateTime;
+
 class Release
 {
     public function __construct(
@@ -11,7 +13,37 @@ class Release
         private string $url,
         private string $name,
         private string $tagName,
-        private \DateTime $createdAt,
-        private \DateTime $publishedAt,
+        private DateTime $createdAt,
+        private DateTime $publishedAt,
     ) {}
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function url(): string
+    {
+        return $this->url;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function tagName(): string
+    {
+        return $this->tagName;
+    }
+
+    public function createdAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function publishedAt(): DateTime
+    {
+        return $this->publishedAt;
+    }
 }
