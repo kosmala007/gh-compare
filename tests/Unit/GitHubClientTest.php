a339adbd8be5c21f096d0ae2d1c8673a248cc34d
Marcin Kosmala <kosmala007@gmail.com>
2022-01-16 21:57:22 +0100

    Remove Guzzle use Symfony http-client

--- tests/Unit/GitHubClientTest.php
+++ tests/Unit/GitHubClientTest.php
@@ -10,13 +10,29 @@ use App\Dto\GitHubRepo;
 use App\Exception\InvalidPullStateException;
 use App\Exception\NotFoundRepoException;
 use App\Exception\NotFoundResourceException;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 
-class GitHubClientTest extends TestCase
+class GitHubClientTest extends WebTestCase
 {
     public function testGetRepo(): void
     {
-        $client = new GitHubClient();
+        $response = json_encode([
+            'id' => 458058,
+            'name' => 'symfony',
+            'full_name' => 'symfony/symfony',
+            'url' => 'https://api.github.com/repos/symfony/symfony',
+            'description' => 'The Symfony PHP framework',
+            'forks_count' => 8505,
+            'stargazers_count' => 26341,
+            'watchers_count' => 26341,
+        ]);
+        $mockResponse = new MockResponse($response, [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $repo = $client->getRepo('symfony/symfony');
 
         $this->assertInstanceOf(GitHubRepo::class, $repo);
@@ -26,13 +42,29 @@ class GitHubClientTest extends TestCase
     {
         $this->expectException(NotFoundRepoException::class);
 
-        $client = new GitHubClient();
+        $mockResponse = new MockResponse(json_encode(null), [
+            'http_code' => 404,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $client->getRepo('test/not-found');
     }
 
     public function testGetLatestRelease(): void
     {
-        $client = new GitHubClient();
+        $response = [[
+            'id' => 56140427,
+            'url' => 'https://api.github.com/repos/symfony/symfony/releases/56140427',
+            'name' => 'v6.0.2',
+            'tag_name' => 'v6.0.2',
+            'created_at' => '2021-12-29T14:07:47Z',
+            'published_at' => '2021-12-29T14:14:15Z',
+        ]];
+        $mockResponse = new MockResponse(json_encode($response), [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $release = $client->getLatestRelease('symfony/symfony');
 
         $this->assertInstanceOf(GitHubRelease::class, $release);
@@ -42,13 +74,21 @@ class GitHubClientTest extends TestCase
     {
         $this->expectException(NotFoundResourceException::class);
 
-        $client = new GitHubClient();
+        $mockResponse = new MockResponse(json_encode(null), [
+            'http_code' => 404,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $release = $client->getLatestRelease('test/not-found');
     }
 
     public function testRepoWithoutRelease(): void
     {
-        $client = new GitHubClient();
+        $mockResponse = new MockResponse(json_encode([]), [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $release = $client->getLatestRelease('octocat/Hello-World');
 
         $this->assertEmpty($release);
@@ -56,7 +96,14 @@ class GitHubClientTest extends TestCase
 
     public function testGetPullsCount(): void
     {
-        $client = new GitHubClient();
+        $response = [
+            'total_count' => 100,
+        ];
+        $mockResponse = new MockResponse(json_encode($response), [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $count = $client->getCountPulls('symfony/symfony');
 
         $this->assertIsInt($count);
@@ -64,7 +111,14 @@ class GitHubClientTest extends TestCase
 
     public function testGetOpenPullsCount(): void
     {
-        $client = new GitHubClient();
+        $response = [
+            'total_count' => 100,
+        ];
+        $mockResponse = new MockResponse(json_encode($response), [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $count = $client->getCountPulls('symfony/symfony', GitHubClient::PULL_STATE_OPEN);
 
         $this->assertIsInt($count);
@@ -72,7 +126,14 @@ class GitHubClientTest extends TestCase
 
     public function testGetClosePullsCount(): void
     {
-        $client = new GitHubClient();
+        $response = [
+            'total_count' => 100,
+        ];
+        $mockResponse = new MockResponse(json_encode($response), [
+            'http_code' => 200,
+        ]);
+        $mockHttpClient = new MockHttpClient($mockResponse);
+        $client = new GitHubClient($mockHttpClient);
         $count = $client->getCountPulls('symfony/symfony', GitHubClient::PULL_STATE_CLOSE);
 
         $this->assertIsInt($count);
@@ -82,7 +143,8 @@ class GitHubClientTest extends TestCase
     {
         $this->expectException(InvalidPullStateException::class);
 
-        $client = new GitHubClient();
+        $mockHttpClient = new MockHttpClient();
+        $client = new GitHubClient($mockHttpClient);
         $release = $client->getCountPulls('test/not-found', 'invalid');
     }
 }
